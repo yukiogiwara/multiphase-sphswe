64cfd427cbb70f32c17c6ce7798febc781c9ba08
yukiogiwara <yukiogiwara.cg@gmail.com>
2022-05-02 02:28:52 +0900

    [add] kernel function pointer

---
+++ inc/type.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file type.hpp
+ * @brief Definition of types
+ * @author Yuki Ogiwara
+ * @date 2022-05-03
+ */
+
+#include <glm/glm.hpp>
+
+// kernel function pointer
+
+using kernel = float (*)(float, float);
+using gkernel = glm::vec2 (*)(glm::vec2, float, float);
+using lkernel = float (*)(float, float);
