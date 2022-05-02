69c31c0b17d97c1604956cf54326365ced8df368
yukiogiwara <yukiogiwara.cg@gmail.com>
2022-05-02 10:50:17 +0900

    [add] interpolation

---
+++ inc/utility.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file utility.hpp
+ * @brief Definition of utility functions
+ * @author Yuki Ogiwara
+ * @date 2022-05-03
+ */
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+#include "type.hpp"
+
+// interpolation with kernel function
+
+float Interpolate(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, unsigned int i, const std::vector<unsigned int> &indices, const kernel &w, float h);
+glm::vec2 GradInterpolate(const std::vector<float> &m, const std::vector<glm::vec2> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, unsigned int i, const std::vector<unsigned int> &indices, const gkernel &w, float h);
+float LaplaceInterpolate(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, unsigned int i, const std::vector<unsigned int> &indices, const lkernel &w, float h);
---
+++ src/utility.cpp
@@ -0,0 +1,71 @@
+/**
+ * @file utility.cpp
+ * @brief Implementation of utility functions
+ * @author Yuki Ogiwara
+ * @date 2022-05-03
+ */
+
+#include "utility.hpp"
+
+/**
+ * @brief interpolation with kernel function
+ * @param[in] m mass
+ * @param[in] phi physical quantity
+ * @param[in] rho density
+ * @param[in] r position
+ * @param[in] i particle index
+ * @param[in] indices neighbor particles
+ * @param[in] w kernel
+ * @param[in] h effective_radius
+ * @return interpolated physical quantity
+ */
+float Interpolate(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, unsigned int i, const std::vector<unsigned int> &indices, const kernel &w, float h) {
+    float val = 0.0f;
+    for(int j : indices) {
+        glm::vec2 r_ij = r[j] - r[i];
+        val += m[j] * phi[j] / rho[j] * w(glm::length(r_ij), h);
+    }
+    return val;
+}
+
+/**
+ * @brief interpolation with kernel function
+ * @param[in] m mass
+ * @param[in] phi physical quantity
+ * @param[in] rho density
+ * @param[in] r position
+ * @param[in] i particle index
+ * @param[in] indices neighbor particles
+ * @param[in] w kernel
+ * @param[in] h effective_radius
+ * @return interpolated gradient of physical quantity
+ */
+glm::vec2 GradInterpolate(const std::vector<float> &m, const std::vector<glm::vec2> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, unsigned int i, const std::vector<unsigned int> &indices, const gkernel &w, float h) {
+    glm::vec2 val = glm::vec2(0.0f, 0.0f);
+    for(int j : indices) {
+        glm::vec2 r_ij = r[j] - r[i];
+        val += m[j] * phi[j] / rho[j] * w(r_ij, glm::length(r_ij), h);
+    }
+    return val;
+}
+
+/**
+ * @brief interpolation with kernel function
+ * @param[in] m mass
+ * @param[in] phi physical quantity
+ * @param[in] rho density
+ * @param[in] r position
+ * @param[in] i particle index
+ * @param[in] indices neighbor particles
+ * @param[in] w kernel
+ * @param[in] h effective_radius
+ * @return interpolated laplacian of physical quantity
+ */
+float LaplaceInterpolate(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, unsigned int i, const std::vector<unsigned int> &indices, const lkernel &w, float h) {
+    float val = 0.0f;
+    for(int j : indices) {
+        glm::vec2 r_ij = r[j] - r[i];
+        val += m[j] * phi[j] / rho[j] * w(glm::length(r_ij), h);
+    }
+    return val;
+}
