2dca296bdae45ca3a4a783ac8747dadea5a3021c
yukiogiwara <yukiogiwara.cg@gmail.com>
2022-05-07 03:10:36 +0900

    [add] constant

---
+++ inc/constant.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp 
+ * @brief Declaration of constants
+ * @author Yuki Ogiwara
+ * @date 2022-05-07
+ */
+
+#include <glm/glm.hpp>
+
+const int kOpenGLVersionMejor = 3;
+const int kOpenGLVersionMinor = 3;
+
+const glm::vec3 kBackgroundColor = glm::vec3(1.0f, 1.0f, 1.0f);
+const float kPointSize = 8.0f;
+
