76c35929f2276954321c20c92ed6384f39c1e356
yukiogiwara <yukiogiwara.cg@gmail.com>
2022-05-07 00:21:56 +0900

    [fix] Interpolate

--- src/utility.cpp
+++ src/utility.cpp
@@ -22,31 +22,10 @@
 float Interpolate(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int i, const std::vector<int> &indices, const kernel &w, float h) {
     float val = 0.0f;
     for(int j : indices) {
-        glm::vec2 r_ij = r[j] - r[i];
+        glm::vec2 r_ij = r[i] - r[j];
         val += m[j] * phi[j] / rho[j] * w(glm::length(r_ij), h);
     }
     return val;
-}
-
-/**
- * @brief interpolation with kernel function
- * @param[in] m mass
- * @param[in, out] phi physical quantity
- * @param[in] rho density
- * @param[in] r position
- * @param[in] n number of particles
- * @param[in] indices neighbor particles
- * @param[in] w kernel
- * @param[in] h effective_radius
- */
-void Interpolate(const std::vector<float> &m, std::vector<float>* phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int n, const std::vector<std::vector<int>> &indices, const kernel &w, float h) {
-    std::vector<float> val(n);
-    for(int i = 0; i < n; i++) {
-        val[i] = Interpolate(m, *phi, rho, r, i, indices[i], w, h);
-    }
-    for(int i = 0; i < n; i++) {
-        phi->at(i) = val[i];
-    }
 }
 
 /**
@@ -61,34 +40,14 @@ void Interpolate(const std::vector<float> &m, std::vector<float>* phi, const std
  * @param[in] h effective_radius
  * @return interpolated gradient of physical quantity
  */
-glm::vec2 InterpolateGradient(const std::vector<float> &m, const std::vector<glm::vec2> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int i, const std::vector<int> &indices, const gkernel &w, float h) {
+glm::vec2 InterpolateGradient(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int i, const std::vector<int> &indices, const gkernel &w, float h) {
     glm::vec2 val = glm::vec2(0.0f, 0.0f);
     for(int j : indices) {
-        glm::vec2 r_ij = r[j] - r[i];
+        if(i == j) continue;
+        glm::vec2 r_ij = r[i] - r[j];
         val += m[j] * phi[j] / rho[j] * w(r_ij, glm::length(r_ij), h);
     }
     return val;
-}
-
-/**
- * @brief interpolation with kernel function
- * @param[in] m mass
- * @param[in, out] phi physical quantity
- * @param[in] rho density
- * @param[in] r position
- * @param[in] n number of particles
- * @param[in] indices neighbor particles
- * @param[in] w kernel
- * @param[in] h effective_radius
- */
-void InterpolateGradient(const std::vector<float> &m, std::vector<glm::vec2>* phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int n, const std::vector<std::vector<int>> &indices, const gkernel &w, float h) {
-    std::vector<glm::vec2> val(n);
-    for(int i = 0; i < n; i++) {
-        val[i] = InterpolateGradient(m, *phi, rho, r, i, indices[i], w, h);
-    }
-    for(int i = 0; i < n; i++) {
-        phi->at(i) = val[i];
-    }
 }
 
 /**
@@ -103,34 +62,13 @@ void InterpolateGradient(const std::vector<float> &m, std::vector<glm::vec2>* ph
  * @param[in] h effective_radius
  * @return interpolated laplacian of physical quantity
  */
-float InterpolateLaplacian(const std::vector<float> &m, const std::vector<float> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int i, const std::vector<int> &indices, const lkernel &w, float h) {
-    float val = 0.0f;
+glm::vec2 InterpolateLaplacian(const std::vector<float> &m, const std::vector<glm::vec2> &phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int i, const std::vector<int> &indices, const lkernel &w, float h) {
+    glm::vec2 val = glm::vec2(0.0f, 0.0f);
     for(int j : indices) {
-        glm::vec2 r_ij = r[j] - r[i];
-        val += m[j] * phi[j] / rho[j] * w(glm::length(r_ij), h);
+        glm::vec2 r_ij = r[i] - r[j];
+        val += m[j] * (phi[j] - phi[i]) / rho[j] * w(glm::length(r_ij), h);
     }
     return val;
-}
-
-/**
- * @brief interpolation with kernel function
- * @param[in] m mass
- * @param[in, out] phi physical quantity
- * @param[in] rho density
- * @param[in] r position
- * @param[in] n number of particles
- * @param[in] indices neighbor particles
- * @param[in] w kernel
- * @param[in] h effective_radius
- */
-void InterpolateLaplacian(const std::vector<float> &m, std::vector<float>* phi, const std::vector<float> &rho, const std::vector<glm::vec2> &r, int n, const std::vector<std::vector<int>> &indices, const lkernel &w, float h) {
-    std::vector<float> val(n);
-    for(int i = 0; i < n; i++) {
-        val[i] = InterpolateLaplacian(m, *phi, rho, r, i, indices[i], w, h);
-    }
-    for(int i = 0; i < n; i++) {
-        phi->at(i) = val[i];
-    }
 }
 
 /**
