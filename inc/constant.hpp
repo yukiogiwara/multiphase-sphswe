6e9ae802d78e641962cfd6f1b302e7bfa09beef3
yukiogiwara <yukiogiwara.cg@gmail.com>
2022-05-07 11:03:27 +0900

    [add] constant

--- inc/constant.hpp
+++ inc/constant.hpp
@@ -6,9 +6,12 @@
  */
 
 #include <glm/glm.hpp>
+#include <string>
 
 const int kOpenGLVersionMejor = 3;
 const int kOpenGLVersionMinor = 3;
+
+const std::string kTitle = "Multiphase SPH Based Shallow Water Simulation";
 
 const glm::vec3 kBackgroundColor = glm::vec3(1.0f, 1.0f, 1.0f);
 const float kPointSize = 8.0f;
